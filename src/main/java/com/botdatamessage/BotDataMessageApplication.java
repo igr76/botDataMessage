49d3ff102c9172cd53f79e8b42806d85922d6414
igr76 <gritsuk766@gmail.com>
2023-11-09 21:59:56 +1000

    start

---
+++ src/main/java/com/botdatamessage/BotDataMessageApplication.java
@@ -0,0 +1,13 @@
+package com.botdatamessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotDataMessageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotDataMessageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/botdatamessage/BotDataMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.botdatamessage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotDataMessageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
